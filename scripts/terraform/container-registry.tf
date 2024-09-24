8f4e143121506673e50d66c2a2a028afe71f61c0
Himani1012 <165289737+Himani1012@users.noreply.github.com>
2024-09-24 16:08:36 +1000

    Create container-registry.tf

---
+++ scripts/terraform/container-registry.tf
@@ -0,0 +1,7 @@
+resource "azurerm_container_registry" "acr" {
+  name                = "distinctionweek9"  
+  resource_group_name = "deakinuni"       # Resource group name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
