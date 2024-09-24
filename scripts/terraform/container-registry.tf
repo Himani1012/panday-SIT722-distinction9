1a1d8c6ce384faa161d5df75e6f49b4808140fad
Himani1012 <165289737+Himani1012@users.noreply.github.com>
2024-09-24 23:18:21 +1000

    Update container-registry.tf

--- scripts/terraform/container-registry.tf
+++ scripts/terraform/container-registry.tf
@@ -4,9 +4,6 @@ resource "azurerm_container_registry" "acr" {
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
   admin_enabled       = true
-
-admin_username = "distinctionweek9"
-  admin_password = var.admin_password // Reference to a variabl
 }
 
 
