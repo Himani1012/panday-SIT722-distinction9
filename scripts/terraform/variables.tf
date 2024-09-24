38de17b77af377be09fab29fa01ee8e7fb4060fd
Himani1012 <165289737+Himani1012@users.noreply.github.com>
2024-09-24 19:54:17 +1000

    Update variables.tf

--- scripts/terraform/variables.tf
+++ scripts/terraform/variables.tf
@@ -1,3 +1,24 @@
 variable "location" {
   default = "australiaeast"
 }
+variable "client_id" {
+  description = "The client ID for the Azure Active Directory application."
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The client secret for the Azure Active Directory application."
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "The tenant ID of the Azure Active Directory."
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "The subscription ID for the Azure resources."
+  type        = string
+}
+
