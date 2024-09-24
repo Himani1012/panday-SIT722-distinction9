b3a78cd23e55cfe4d632343239142600efbe10cb
Himani1012 <165289737+Himani1012@users.noreply.github.com>
2024-09-24 20:29:10 +1000

    Update providers.tf

--- scripts/terraform/providers.tf
+++ scripts/terraform/providers.tf
@@ -6,7 +6,9 @@ terraform {
     }
   }
 }
+
 provider "azurerm" {
+  features {}  # Add this line
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
