728cb6aa871f9da3a42d390ed6ce926ef084c78d
Himani1012 <165289737+Himani1012@users.noreply.github.com>
2024-09-24 16:09:07 +1000

    Create kubernetes-cluster.tf

---
+++ scripts/terraform/kubernetes-cluster.tf
@@ -0,0 +1,27 @@
+resource "azurerm_kubernetes_cluster" "credittaskweek8" {
+  name                = "distinctionweek9"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "distinctionweek9-dns"
+
+  default_node_pool {
+    name       = "agentpool"          # Name of the default node pool
+    node_count = 1                  # Number of nodes in the node pool
+    vm_size    = "Standard_D2s_v3" # VM size for nodes
+   
+  }
+
+  identity {
+    type = "SystemAssigned"         # Identity type for the cluster
+  }
+
+  kubernetes_version = "1.29.7"     # Kubernetes version
+  tags = {
+    environment = "test"
+  }
+}
+
+/* output "kube_config" {
+  value     = azurerm_kubernetes_cluster.himanipart4.kube_config_raw
+  sensitive = true
+} */
