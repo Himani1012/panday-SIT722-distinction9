e65ab9d66830b68f924fdf446578e97d53e6e3c2
Himani1012 <165289737+Himani1012@users.noreply.github.com>
2024-09-25 00:12:40 +1000

    Update variables.tf

--- scripts/terraform/variables.tf
+++ scripts/terraform/variables.tf
@@ -1,3 +1,42 @@
 variable "location" {
   default = "australiaeast"
 }
+variable "ARM_CLIENT_ID" {
+  type        = string
+  description = "Azure Resource Manager Client ID"
+}
+
+variable "ARM_CLIENT_SECRET" {
+  type        = string
+  description = "Azure Resource Manager Client Secret"
+}
+
+variable "ARM_TENANT_ID" {
+  type        = string
+  description = "Azure Resource Manager Tenant ID"
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+  type        = string
+  description = "Azure Resource Manager Subscription ID"
+}
+
+variable "ADMIN_PASSWORD" {
+  type        = string
+  description = "Admin password for Azure Container Registry"
+}
+
+variable "CONTAINER_REGISTRY" {
+  type        = string
+  description = "Container Registry URL"
+}
+
+variable "REGISTRY_UN" {
+  type        = string
+  description = "Registry Username"
+}
+
+variable "REGISTRY_PW" {
+  type        = string
+  description = "Registry Password"
+}
