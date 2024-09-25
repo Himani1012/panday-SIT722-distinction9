5940af86ca15da0dac904117d72a4cad75d62f8d
Himani1012 <165289737+Himani1012@users.noreply.github.com>
2024-09-25 20:46:31 +1000

    Update container-registry.tf

--- scripts/terraform/container-registry.tf
+++ scripts/terraform/container-registry.tf
@@ -1,4 +1,4 @@
-resource "azurerm_container_registry" "acr" {
+resource "azurerm_container_registry" "distinctionweek9" {
   name                = "distinctionweek9"  
   resource_group_name = "deakinuni"       # Resource group name
   location            = azurerm_resource_group.rg.location
